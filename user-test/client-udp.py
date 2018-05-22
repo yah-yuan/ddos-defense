09180fdd0ee2dc9b93c124362558b163fb051888
yahweh <0505yahweh@gmail.com>
2018-05-22 18:16:04 +0800

    delete

--- user-test/client-udp.py
+++
@@ -1,9 +0,0 @@
-import socket 
-import time 
-    
-address = ('192.168.2.129', 33333)  
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
-    
-while True: 
-    s.sendto(b'Hey there, this is a UDP flow', address)  
-    time.sleep(1)
